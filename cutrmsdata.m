68f02828e6be706d46baaf045ea0de7a2c99ead6
Yuri Tamama <ytamama@cressida.princeton.edu>
2020-10-10 18:18:46 -0400

    Minor adjustments

--- cutrmsdata.m
+++ cutrmsdata.m
@@ -1,5 +1,5 @@
-function [newtimevec,outputz,outputy,outputx]=cutrmsdata(...
-    oldtimevec,inputz,inputy,inputx,starttime,endtime,timezone,rmstype)
+function [newtimevec,outputz,outputy,outputx,outputh]=cutrmsdata(...
+    oldtimevec,inputz,inputy,inputx,inputh,starttime,endtime,timezone,rmstype)
 % 
 % Function to crop the inputted data, with each point corresponding to
 % a particular time, to a specified time interval.
@@ -7,12 +7,15 @@ function [newtimevec,outputz,outputy,outputx]=cutrmsdata(...
 % INPUTS
 % oldtimevec : The vector of datetimes, with each datetime corresponding
 %              to an input data point
-% inputz: A matrix listing the RMS displacement/velocity/acceleration 
-%         in the Z (vertical) component.
-% inputy: A matrix listing the RMS displacement/velocity/acceleration 
-%         in the Y (North-South) component.
-% inputx: A matrix listing the RMS displacement/velocity/acceleration 
-%         in the X (East-West) component.
+% inputz : A matrix listing the RMS displacement/velocity/acceleration 
+%          in the Z (vertical) component.
+% inputy : A matrix listing the RMS displacement/velocity/acceleration 
+%          in the Y (North-South) component.
+% inputx : A matrix listing the RMS displacement/velocity/acceleration 
+%          in the X (East-West) component.
+% inputh : A matrix listing the RMS displacement/velocity/acceleration 
+%          in the H (horizontal) component. Leave empty if we only want 
+%          X and Y.
 % starttime : From what time do we start our time series? Input a 
 %             number from 0-23 signifying an hour if plotting 'qrt' 
 %             data (rmstype=2). Otherwise enter a datetime vector, with
@@ -38,11 +41,16 @@ function [newtimevec,outputz,outputy,outputx]=cutrmsdata(...
 %           cropped so that each point corresponds to newtimevec.
 % outputx : A matrix listing the RMS disp/vel/acc in the X component, 
 %           cropped so that each point corresponds to newtimevec.
+% outputh : A matrix listing the RMS disp/vel/acc in the H component, 
+%           cropped so that each point corresponds to newtimevec. An empty
+%           vector will be displayed if inputh is not entered.
+%
+% NOTE: This function cuts the time vector according to start and end time
 %
 % Reference
 % Uses defval.m, in csdms-contrib/slepian_alpha 
 %
-% Last Modified by Yuri Tamama, 09/02/2020
+% Last Modified by Yuri Tamama, 10/10/2020
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 defval('timezone','UTC')
 
@@ -50,19 +58,25 @@ defval('timezone','UTC')
 rmsx17=[];
 rmsy17=[];
 rmsz17=[];
+rmsh17=[];
 rmsx18=[];
 rmsy18=[];
 rmsz18=[];
+rmsh18=[];
 rmsx19=[];
 rmsy19=[];
 rmsz19=[];
+rmsh19=[];
 rmsx20=[];
 rmsy20=[];
 rmsz20=[];
+rmsh20=[];
 newtimevec=[];
 for i=1:length(oldtimevec)
   temptime=oldtimevec(i);
   temptime.TimeZone=timezone;
+  % Variable to check whether a particular time is within the range of
+  % times that we want
   timeadded=0;
   if rmstype==2
     if (temptime.Hour>=starttime) && (temptime.Hour<=endtime)
@@ -88,6 +102,12 @@ for i=1:length(oldtimevec)
     rmsx20=[rmsx20; inputx(i,4)];
     rmsy20=[rmsy20; inputy(i,4)];
     rmsz20=[rmsz20; inputz(i,4)];
+    if ~isempty(inputh)
+      rmsh17=[rmsh17; inputh(i,1)];
+      rmsh18=[rmsh18; inputh(i,2)];
+      rmsh19=[rmsh19; inputh(i,3)];
+      rmsh20=[rmsh20; inputh(i,4)];
+    end  
   end
 end
 outputx=zeros(length(newtimevec),4);
@@ -107,6 +127,16 @@ outputz(:,1)=rmsz17;
 outputz(:,2)=rmsz18;
 outputz(:,3)=rmsz19;
 outputz(:,4)=rmsz20;
+%
+if ~isempty(inputh)
+  outputh=zeros(length(newtimevec),4);
+  outputh(:,1)=rmsh17;
+  outputh(:,2)=rmsh18;
+  outputh(:,3)=rmsh19;
+  outputh(:,4)=rmsh20;
+else
+  outputh=[];
+end  
 
 
 
